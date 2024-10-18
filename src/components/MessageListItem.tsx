d062dc4f8f733d090f8126cc95a3a936c0037e61
Eli Tamosauskas <tamosauskas09@gmail.com>
2024-10-18 18:47:53 +0300

    progress

--- src/components/MessageListItem.tsx
+++ src/components/MessageListItem.tsx
@@ -1,10 +1,7 @@
-import {
-  IonItem,
-  IonLabel,
-  IonNote
-  } from '@ionic/react';
-import { Message } from '../data/messages';
-import './MessageListItem.css';
+import { IonChip, IonIcon, IonItem, IonLabel, IonNote } from "@ionic/react";
+import { Message } from "../data/messages";
+import "./MessageListItem.css";
+import { closeCircle } from "ionicons/icons";
 
 interface MessageListItemProps {
   message: Message;
@@ -12,19 +9,36 @@ interface MessageListItemProps {
 
 const MessageListItem: React.FC<MessageListItemProps> = ({ message }) => {
   return (
-    <IonItem id="message-list-item" routerLink={`/message/${message.id}`} detail={false}>
-      <div slot="start" className="dot dot-unread"></div>
+    <IonItem
+      id="message-list-item"
+      routerLink={`/message/${message.id}`}
+      detail={false}
+    >
       <IonLabel className="ion-text-wrap">
         <h2>
-          {message.fromName}
+          <h3>🤖 {message.fromName}</h3>
           <span className="date">
             <IonNote>{message.date}</IonNote>
           </span>
         </h2>
-        <h3>{message.subject}</h3>
+
+        <h2>{message.subject}</h2>
+
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
+
+        <div>
+          <IonChip>💬 7 Comments</IonChip>
+          <IonChip>⬆️ 3</IonChip>
+          <IonChip>⬇️ 0</IonChip>
+        </div>
       </IonLabel>
     </IonItem>
   );
