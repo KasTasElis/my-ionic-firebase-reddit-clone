09b7f44add5a25197a2a1efe8f535a88e76d802d
Eli Tamosauskas <tamosauskas09@gmail.com>
2025-03-01 17:43:28 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,8 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
+// test
+
 /**
  * Ionic Dark Mode
  * -----------------------------------------------------
